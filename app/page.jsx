f9a5ae91329f80891ffda27e4ca8a17f1efc1dc7
Edonak <edouardnakasila69@gmail.com>
2024-12-13 10:56:50 +0100

    barre de navitation et toute les animation finies

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,12 @@
+import { Section } from "lucide-react";
+
 export default function Home() {
   return (
     <main>
-      <p>Home page</p>
+      <section className="bg-white w-full h-[92vh] text-black">
+        <p>HELLO</p>
+      </section>
+      
     </main>
   );
 }
