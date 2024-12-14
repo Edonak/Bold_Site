205a2f3b5d743f1a05ad3c748d9c18f41f39a13d
Edonak <edouardnakasila69@gmail.com>
2024-12-14 14:55:24 +0100

    demarage de la page d'accueil

--- components/Herosection.jsx
+++ components/Herosection.jsx
@@ -7,14 +7,14 @@ export default function HeroSection() {
       <section className="w-full h-auto bg-red-900 text-white py-12 px-6 md:px-16 flex flex-col md:flex-row items-center justify-between">
         {/* Texte principal */}
         <div className="flex flex-col items-center md:items-start max-w-md">
-          <h1 className="text-5xl md:text-6xl font-bold text-yellow-500 mb-6">
+          <h1 className="text-5xl md:text-6xl font-bold text-yellow-500 mb-6" style={{ fontFamily: 'sans-serif'}}>
             BOLD
           </h1>
           <p className="text-lg md:text-xl text-center md:text-left leading-relaxed">
             Ensemble Valorisons la voix des Femmes et des Filles et construisons
             leur un espace d'expression ouverte et positive.
           </p>
-          <Button className="mt-8 px-6 py-3 bg-accent hover:bg-yellow-500 hover:text-red-900 transition">Faire un don</Button>
+          <Button className="mt-8 px-6 py-3 bg-transparent border-white text-white hover:bg-yellow-500 hover:text-red-900 transition">Faire un don</Button>
         </div>
   
         {/* Image */}
