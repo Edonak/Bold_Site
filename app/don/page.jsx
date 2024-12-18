5b7f058f6469cdcda832b61b1c764d36c1c7246e
Edonak <edouardnakasila69@gmail.com>
2024-12-18 11:19:07 +0100

    about component on the home page finish

--- app/don/page.jsx
+++ app/don/page.jsx
@@ -1,10 +1,11 @@
 import Footer from '@/components/Footer'
 import React from 'react'
 import DonationForm from './FormDon'
+import HeroSectionDon from './HeroSectionDon'
 export default function page() {
   return (
     <>
-    <div>Faire un don</div>
+    <HeroSectionDon />
     <DonationForm />
     <Footer />
     </>
--- components/aboutComponent.jsx
+++ components/aboutComponent.jsx
@@ -1,16 +1,18 @@
+import { Button } from "./ui/button";
+
 export default function AboutComponent() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-white md:px-12">
-      <div className="flex flex-col md:flex-row items-center gap-8 max-w-5xl">
-        <div className="relative w-[100%]">
-          <div className="w-[100%] h-[100%] md:w-72 md:h-72 rounded-full overflow-hidden">
+      <div className="flex flex-col md:flex-row items-center gap-12 max-w-6xl">
+        <div className="relative w-[100%] flex justify-center">
+          <div className="w-80 h-80 md:w-96 md:h-96 rounded-full overflow-hidden">
             <img
               src="/images/AboutBoldFirstimage.jpg"
               alt="Équipe principale"
               className="w-full h-full object-cover"
             />
           </div>
-          <div className="absolute bottom-0 left-0 w-32 h-32 md:w-36 md:h-36 rounded-full overflow-hidden translate-x-5 translate-y-10">
+          <div className="absolute bottom-0 left-0 w-40 h-40 md:w-48 md:h-48 rounded-full overflow-hidden translate-x-24 translate-y-14">
             <img
               src="/images/AboutBoldSecondImage.jpg"
               alt="Activité"
@@ -18,31 +20,29 @@ export default function AboutComponent() {
             />
           </div>
         </div>
-        <div className="text-justify md:text-left ">
-          <h1 className="text-4xl md:text-5xl font-bold text-gray-900 leading-tight ">
+        <div className="text-justify md:text-left">
+          <h1 className="text-4xl md:text-5xl font-bold text-gray-900 leading-tight mb-4">
             Bienvenue à <span className="text-[#8F1D07] font-blanquotey">BOLD</span>
           </h1>
-          <p className="text-lg md:text-xl text-gray-700 mt-4">
-            <strong>BOLD</strong> est une Association Sans But Lucratif
-            poursuivant la vision d’un monde de dignité pour toutes les femmes
-            et les filles.
+          <p className="text-lg md:text-xl text-gray-700 mt-4 text-justify">
+            <strong>BOLD</strong> est une Association Sans But Lucratif poursuivant la vision d’un
+            monde de dignité pour toutes les femmes et les filles.
           </p>
-          <p className="text-lg md:text-xl text-gray-700 mt-4">
-            Notre mission est de renforcer, accompagner et soutenir les femmes
-            et filles pour la construction des communautés inclusives et
-            équitables.
+          <p className="text-lg md:text-xl text-gray-700 mt-4 text-justify">
+            Notre mission est de renforcer, accompagner et soutenir les femmes et filles pour la
+            construction des communautés inclusives et équitables.
           </p>
-          <p className="text-lg md:text-xl text-gray-700 mt-4">
-            Son siège social se situe à Goma, en République Démocratique du
-            Congo.
+          <p className="text-lg md:text-xl text-gray-700 mt-4 text-justify">
+            Son siège social se situe à Goma, en République Démocratique du Congo.
           </p>
-          <div className="mt-6">
-            <a
+          <div className="flex justify-center mt-8">
+            {/* <a
               href="#about"
-              className="px-6 py-3 bg-red-500 text-white text-lg font-medium rounded-full shadow-md hover:bg-red-600 transition"
+              className="px-8 py-4 bg-transparent text-[#8F1D07] border text-lg font-medium rounded-full shadow-md hover:bg-red-600 transition"
             >
               A propos
-            </a>
+            </a> */}
+             <Button className='bg-transparent text-black border-[#8F1D07]'>A propos</Button>
           </div>
         </div>
       </div>
